7ff4a3f3e48f439d6c5aa4115f08464cb27a7647
Trang Nguyen <fandymees@gmail.com>
2023-05-28 12:21:01 +0700

    tao router,api tai khoan khach hang(user)

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,8 +1,11 @@
 const siteRouter = require('./siteRouter');
 const contactRouter = require('./contactRouter');
+const userRouter = require('./userRouter');
 const boardgameRouter = require('./boradgameRouter');
 
+
 function route(app){
+    app.use('/user', userRouter);
     app.use('/contact', contactRouter);
     app.use('/store', boardgameRouter);
     
