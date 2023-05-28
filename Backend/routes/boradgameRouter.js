9039266fa89354dd09757c27db63173a333c58c9
Hoang Vu <vmhoang1703@gmail.com>
2023-05-28 18:26:11 +0700

    #111 API endpoint

--- Backend/routes/boradgameRouter.js
+++ Backend/routes/boradgameRouter.js
@@ -3,8 +3,6 @@ var router = express.Router();
 
 const boardgameController = require('../controllers/boardgameController');
 
-router.get('/order', boardgameController.order);
-router.get('/:id', boardgameController.show);
 router.get('/', boardgameController.index);
 
 module.exports = router;
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -3,10 +3,7 @@ const contactRouter = require('./contactRouter');
 const boardgameRouter = require('./boradgameRouter');
 
 function route(app){
-    app.use('/contact', contactRouter);
     app.use('/store', boardgameRouter);
-    
-    app.use('/', siteRouter);
 }
 
 module.exports = route;
