8c7e6fb58e62f97b566e04788d83bfb8589902f8
Hoang Vu <vmhoang1703@gmail.com>
2023-05-28 20:33:49 +0700

    #114 code phan trang js

--- Backend/controllers/boardgameController.js
+++ Backend/controllers/boardgameController.js
@@ -7,8 +7,16 @@ class boardgameController {
     async index(req, res, next) {
         try {
             const boardgames = await Boardgame.find({});
+            const itemsPerPage = 20; // Số sản phẩm trên mỗi trang
+            const currentPage = req.query.page || 1; // Trang hiện tại (mặc định là 1)
+            const startIndex = (currentPage - 1) * itemsPerPage;
+            const endIndex = currentPage * itemsPerPage;
+    
+            const boardgamesPage = boardgames.slice(startIndex, endIndex);
             res.render('layout/main', {
-                boardgames: multipleMongooseToObject(boardgames),
+                boardgames: multipleMongooseToObject(boardgamesPage),
+                currentPage,
+                totalPages: Math.ceil(boardgames.length / itemsPerPage)
             });
         } catch (error) {
             next(error);
