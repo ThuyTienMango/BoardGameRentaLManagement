47d9b7c7d8abd5122e93904a40187b4737684f49
Trang Nguyen <fandymees@gmail.com>
2023-05-30 16:49:13 +0700

    #125 update

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -6,7 +6,9 @@ class orderController {
     //[GET] /order/:id
     async order(req, res, next){
         try{
+            const selectedDuration = req.body.bookingduration;
             const boardgame = await Boardgame.findById(req.params.id);
+            console.log(selectedDuration);
             res.render('boardgames/order', { 
                 boardgame: mongooseToObject(boardgame),
              });
--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const db = require('./db');
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //HTTP logger
 app.use(morgan('combined'));
