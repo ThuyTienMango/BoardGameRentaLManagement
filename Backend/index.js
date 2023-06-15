fa91350898adecbfe21fecbe9b6fc22f05d8654d
TrangNguyen0909 <fandymees@gmail.com>
2023-06-15 10:31:50 +0700

    hien anh khi lưu

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -7,10 +5,6 @@ const path = require('path');
 
 const db = require('./db');
 const route = require('./routes');
-
-
-//Connect to DB
-db.connect();
 
 const app = express();
 
@@ -20,13 +14,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//HTTP logger
+// Kết nối tới cơ sở dữ liệu
+db.connect();
+
+// Middleware ghi log HTTP
 app.use(morgan('combined'));
 
-//Routes init
+// Khởi tạo các route
 route(app);
 
-
-app.listen(3000, function(req, res){
-    console.log('Server started on port 3000');
-})
+app.listen(3000, function(req, res) {
+  console.log('Server started on port 3000');
+});
--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const userController = require('../controllers/userController');
 
+
+router.get('/register', userController.reg);
 router.post('/save-customer-info', userController.save);
-router.get('/register',userController.reg);
 router.get('/:id', userController.show);
 
 module.exports = router;
