0cd45d9380ab7b9bfbc0d4093f9ef004e15cb558
vmhoang1703 <vmhoang1703@gmail.com>
2023-06-18 22:54:36 +0700

    update FE

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -9,6 +9,10 @@ class orderController {
         try{
             const user = await User.findOne({ _id: req.session.user }); // Tìm người dùng theo ID
             const boardgame = await Boardgame.findById(req.params.id);
+            const duration = req.body.bookingduration; // Lấy giá trị duration từ form
+            const quantity = req.body.quantity; // Lấy giá trị quantity từ form
+            console.log(duration);
+            console.log(quantity);
             res.render('boardgames/order', { 
                 boardgame: mongooseToObject(boardgame),
                 user: user,
