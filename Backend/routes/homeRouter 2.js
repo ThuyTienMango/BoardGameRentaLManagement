d7436a65d456b2c2fea3e6da90966348a1bf2b3d
ThuyTienMango <21521526@gm.uit.edu.vn>
2023-07-02 15:37:25 +0700

    xoá các file lỗi do chạy demo

--- Backend/routes/homeRouter 2.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const homeController = require('../controllers/homeController');
-const authController = require('../controllers/authController');
-
-router.get('/', authController.checkLogin, homeController.index);
-
-module.exports = router;
